bef469819e3c00dda38b6f1f2c48f28589a227c5
Levine Hua <951995489@qq.com>
2021-12-25 21:52:01 +0800

    test

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,10 +1,10 @@
 import type { App } from 'vue';
 import ElTest from './el-test';
-import CbMenu from './el-menu';
+import ElMenu from './el-menu';
 import ElForm, { useForm } from './el-form';
 declare const version = "0.0.0-development";
 declare const install: (app: App) => void;
-export { ElTest, ElForm, CbMenu, version, install, };
+export { ElTest, ElForm, ElMenu, version, install, };
 export { useForm, };
 declare const _default: {
     version: string;
--- packages/menu/src/index.vue
+++ packages/menu/src/index.vue
@@ -3,12 +3,12 @@
  * @Author: 华松林
  * @Date: 2021-08-06 15:31:29
  * @LastEditors: 华松林
- * @LastEditTime: 2021-12-25 21:22:25
+ * @LastEditTime: 2021-12-25 21:51:09
  * @FilePath: /hsl-ui/packages/menu/src/index.vue
 -->
 <template>
   <div class="cb-menu">
-    cb-menu
+    menu
   </div>
 </template>
 <script lang="ts">
