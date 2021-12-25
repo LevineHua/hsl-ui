3f744a7884d66b400bfc3c15f9b05c6a8d90b6e9
Levine Hua <951995489@qq.com>
2021-12-25 22:24:19 +0800

    test

--- packages/header/src/index.vue
+++ packages/header/src/index.vue
@@ -3,11 +3,11 @@
  * @Author: 华松林
  * @Date: 2021-12-18 21:45:48
  * @LastEditors: 华松林
- * @LastEditTime: 2021-12-25 21:58:22
+ * @LastEditTime: 2021-12-25 22:22:59
  * @FilePath: /hsl-ui/packages/header/src/index.vue
 -->
 <template>
-  <div>我是header</div>
+  <div>我是cb-header</div>
 </template>
 <script lang='ts'>
 import { defineComponent } from 'vue'
